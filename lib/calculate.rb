c85fe57f79e4501eef56b07d9d58ab98097aa65a
Anshul Vyas <anshvyas@Anshuls-MacBook-Pro.local>
2018-07-24 11:11:11 +0530

    Linting done using rubocop

--- lib/calculate.rb
+++ lib/calculate.rb
@@ -1,86 +1,81 @@
-
+# The calculator class
 class Calculator
+  def initialize; end
 
-	def initialize()
-	end
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(',').map(&:to_i)
+      numbers.inject(0) { |result, element| result + element }
 
-	def self.add(input)
-		if input.empty?
-			0
-		else
-			numbers = input.split(",").map { |num| num.to_i }
-			numbers.inject(0){|result,element| result+element}
+    end
+  end
 
-		end
-	end
+  def self.sub(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(',').map(&:to_i)
+      result = numbers[0] - numbers[1]
+    end
+  end
 
-	def self.sub(input)
-		if input.empty?
-			0
-		else
-			numbers=input.split(",").map{ |num| num.to_i}
-			result=numbers[0]-numbers[1]
-		end
-	end
+  def self.mul(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(',').map(&:to_i)
+      numbers.inject(1) { |result, element| result * element }
+    end
+  end
 
-	def self.mul(input)
-		if input.empty?
-			0
-		else
-			numbers = input.split(",").map { |num| num.to_i }
-			numbers.inject(1){|result,element| result*element}
-		end
-	end
+  def self.div(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(',').map(&:to_i)
+      result = numbers[0] / numbers[1]
+    end
+  end
 
-	def self.div(input)
-		if input.empty?
-			0
-		else
-			numbers = input.split(",").map { |num| num.to_i }
-			result=numbers[0]/numbers[1]
-		end
-	end
-
-	def self.quit()
-		abort("Thank You")
-	end
-
-
+  def self.quit
+    abort('Thank You')
+  end
 end
 
-def main()
-	
-	puts"Welcome to Calculator. Kindly Enter Your Choice"
-	puts" add - Addition"
-	puts" sub - Subtraction"
-	puts" mul - Multiply"
-	puts" div - Divison"
-	puts" quit - Exit"
-
-	puts "Kindly Enter your choice :"
-	choice = gets.chomp
-
-	if choice == 'quit'
-		Calculator.quit()
-	end
-	puts "Kindly Enter the numbers Separated by , :"
-	numbers=gets.chomp
-
-	case choice
-	when "add"
-		puts "Result #{Calculator.add(numbers)}"
-	when "sub"
-		puts "Result #{Calculator.sub(numbers)}"
-	when "mul"
-		puts "Result #{Calculator.mul(numbers)}"
-	when "div"
-		puts "Result #{Calculator.div(numbers)}"
-	else
-		puts"Sorry Wrong Choice!"	
-	end
-
+def menu
+  puts 'Welcome to Calculator. Kindly Enter Your Choice'
+  puts' add - Addition'
+  puts' sub - Subtraction'
+  puts' mul - Multiply'
+  puts' div - Divison'
+  puts' quit - Exit'
+  puts 'Kindly Enter your choice :'
 end
 
-if __FILE__ == $0
-main()
+def choose(choice)
+  Calculator.quit if choice == 'quit'
+  puts 'Kindly Enter the numbers Separated by , :'
+  numbers = gets.chomp
+  case choice
+  when 'add'
+    puts "Result #{Calculator.add(numbers)}"
+  when 'sub'
+    puts "Result #{Calculator.sub(numbers)}"
+  when 'mul'
+    puts "Result #{Calculator.mul(numbers)}"
+  when 'div'
+    puts "Result #{Calculator.div(numbers)}"
+  else
+    puts 'Sorry Wrong Choice!'
+  end
 end
+
+def main
+  menu
+  choice = gets.chomp
+  choose(choice)
+end
+
+main if $PROGRAM_NAME == __FILE__
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,70 +1,67 @@
-require "calculate"
+require 'calculate'
 
 describe Calculator do
-	
-	describe ".add" do
-
-    context "given an empty string" do
-      it "returns zero" do
-        expect(Calculator.add("")).to eql(0)
+  describe '.add' do
+    context 'given an empty string' do
+      it 'returns zero' do
+        expect(Calculator.add('')).to eql(0)
       end
     end
-    	
-    context"given 2 and 4 as input" do
-		it "returns6"do
-		expect(Calculator.add("2,4")).to eql(6)
-		end
-	end
+
+    context 'given 2 and 4 as input' do
+      it 'returns6' do
+        expect(Calculator.add('2,4')).to eql(6)
+      end
+    end
   end
 
-  describe ".sub" do
+  describe '.sub' do
+    context 'given empty string' do
+      it 'return zero' do
+        expect(Calculator.sub('')).to eql(0)
+      end
+    end
 
-  	context "given empty string" do
-  		it "return zero" do
-  			expect(Calculator.sub("")).to eql(0)
-  		end
-  	end
-
-  	context "given 2 and 4 " do
-  		it "returns -2"do
-  		expect(Calculator.sub("2,4")).to eql(-2)
-  	end
-  end
-end
-
-  describe ".mul"do
-  	context "given empty string"do
-  		it "return zero"do
-  			expect(Calculator.mul("")).to eql(0)
-		end
-	end
-
-	context "given 2 and 4 as input" do
-		it "return 8"do
-			expect(Calculator.mul("2,4")).to eql(8)
-		end
-	end	
+    context 'given 2 and 4 ' do
+      it 'returns -2' do
+        expect(Calculator.sub('2,4')).to eql(-2)
+      end
+    end
   end
 
-  describe ".div"do
-  	context "given empty string"do
-  		it "return 0"do
-  			expect(Calculator.div("")).to eql(0)
-		end
-	end
+  describe '.mul' do
+    context 'given empty string' do
+      it 'return zero' do
+        expect(Calculator.mul('')).to eql(0)
+      end
+    end
 
-	context "given 4 and 2 as input"do
-		it"return 2"do
-			expect(Calculator.div("4,2")).to eql(2)
-		end
-	end
+    context 'given 2 and 4 as input' do
+      it 'return 8' do
+        expect(Calculator.mul('2,4')).to eql(8)
+      end
+    end
   end
 
-  describe ".quit"do
-  	context"given quit"do
-  		it "it aborts execution"do
-  			expect(Calculator.quit).to eql("Thank You!!")
-  		end
-  	end
+  describe '.div' do
+    context 'given empty string' do
+      it 'return 0' do
+        expect(Calculator.div('')).to eql(0)
+      end
+    end
+
+    context 'given 4 and 2 as input' do
+      it 'return 2' do
+        expect(Calculator.div('4,2')).to eql(2)
+      end
+    end
+  end
+
+  describe '.quit' do
+    context 'given quit' do
+      it 'it aborts execution' do
+        expect(Calculator.quit).to eql('Thank You!!')
+      end
+    end
   end
 end
